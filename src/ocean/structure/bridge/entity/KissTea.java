776b269c091a2c0e113b38e9044bfa8937189a57
Ocean <2080917477@qq.com>
2022-08-18 16:10:07 +0800

    桥接模式

--- src/ocean/structure/bridge/entity/KissTea.java
+++ src/ocean/structure/bridge/entity/KissTea.java
@@ -4,6 +4,7 @@ import ocean.structure.bridge.advance.RefinedAbstractTea;
 import ocean.structure.bridge.basic.Discount;
 import ocean.structure.bridge.basic.Size;
 
+// 使得抽象和实现可以沿着各自的维度来进行变化，不再是固定的绑定关系。
 public class KissTea extends RefinedAbstractTea {   //创建一个啵啵芋圆奶茶的子类
 
     protected KissTea(Size size, Discount discount) {   //在构造时需要指定具体的大小实现
